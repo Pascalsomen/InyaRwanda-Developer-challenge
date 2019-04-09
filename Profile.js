6c3ecb0d204a9cca4b35610acebdb02de522d5e7
Pascalsomen <37747361+Pascalsomen@users.noreply.github.com>
2019-04-09 15:39:56 +0200

    Add files via upload

---
+++ Profile.js
@@ -0,0 +1,146 @@
+import React from 'react';
+import {Component} from 'react'
+import { StyleSheet,  View,TextInput, Image ,FlatList,Alert,ActivityIndicator } from 'react-native';
+import {AsyncStorage} from 'react-native';
+import { Container, Content, List, ListItem,Left,Body, InputGroup, Input, Icon,Text, Picker, Button, Textarea, Item, Thumbnail } from 'native-base';
+class ProfileScreen extends Component {
+  static navigationOptions = {
+    title: 'My Profile',
+     
+
+  };
+
+  checkUser  = async () => {
+    try {
+      const value = await AsyncStorage.getItem('user');
+      if (value !== null) {
+        
+        this.setState({
+          user: value
+        })
+    
+      }else{
+        this.props.navigation.navigate('LoginScreen');
+      }
+    } catch (error) {
+      // Error retrieving data
+    }
+  }; 
+  async logOut() {
+    try {
+      await AsyncStorage.removeItem('user');
+      this.props.navigation.navigate('LoginScreen');
+      return true;
+    }
+    catch(exception) {
+      return false;
+    }
+  }
+ 
+  constructor(props)
+  {
+ 
+    super(props);
+    
+    this.state = { 
+    
+        
+
+        user: '',
+        comment: ''
+    
+    }
+ 
+  }
+
+  componentDidMount() {
+    
+       return fetch('http://somen.pe.hu/mobileApp/selected.php',{
+       method: 'POST',
+       headers: {
+         'Accept': 'application/json',
+         'Content-Type': 'application/json',
+       },
+       body: JSON.stringify({
+         post_id : this.state.Story
+       })
+     })
+         .then((response) => response.json())
+         .then((responseJson) => {
+           this.setState({
+             loading: false,
+             dataSource: responseJson
+           }, function() {
+             // In this block you can do something with new state.
+           });
+         }),
+         fetch('http://somen.pe.hu/mobileApp/com_selected.php',{
+       method: 'POST',
+       headers: {
+         'Accept': 'application/json',
+         'Content-Type': 'application/json',
+       },
+       body: JSON.stringify({
+         post_id : this.state.Story
+       })
+     })
+         .then((response) => response.json())
+         .then((responseJson) => {
+           this.setState({
+             loading: false,
+             data: responseJson
+           }, function() {
+             // In this block you can do something with new state.
+           });
+         })
+         .catch((error) => {
+           console.error(error);
+         })   
+    }
+  
+  render() {
+    this.checkUser();
+    const { navigation } = this.props;
+  
+    return (
+     <Container style={styles.container}>
+            <Content>
+               
+            <Thumbnail circular source={{ uri: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png' }} style={{height:100,width:null}} />
+              <Text style={{padding:10,fontSize:20}}>{this.state.user}</Text>
+              <Button  onPress={this.logOut}light><Text>Sign Out</Text></Button>
+            </Content>
+
+            </Container>
+    );
+  }
+}
+export default ProfileScreen
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop:50
+  },
+  TextInputStyleClass: {
+ 
+    
+
+    borderWidth: 1,
+    borderColor:'black',
+    color:'black',
+    padding:10,
+    borderColor:'#E1E1E1'
+     
+    },
+     
+     TextComponentStyle: {
+      fontSize: 20,
+      color: "#000",
+      textAlign: 'center', 
+      marginBottom: 15
+     }
+});
---
+++ Register.js
@@ -0,0 +1,134 @@
+import React from 'react';
+import {Component} from 'react'
+import { StyleSheet,  View,TextInput, Alert,AsyncStorage } from 'react-native';
+import { Container, Content, List, ListItem, InputGroup, Input, Icon, Text, Picker, Button } from 'native-base';
+class RegisterScreen extends Component {
+  static navigationOptions = {
+    title: 'New Account',
+
+
+  };
+  constructor(props) {
+ 
+    super(props)
+
+       this.state = {
+           
+            user: '',
+            pass: ''
+
+
+                 }
+         }
+         storeData = async () => {
+          const { user }  = this.state ;
+          try {
+            await AsyncStorage.setItem('user', user);
+          } catch (error) {
+            // Error saving data
+          }
+        };  
+create = () =>{
+
+
+
+const { user }  = this.state ;
+const { pass }  = this.state ;
+
+if (this.state.user.trim() != "" && this.state.pass.trim() != "") {
+fetch('http://somen.pe.hu/mobileApp/Register.php', {
+method: 'POST',
+headers: {
+'Accept': 'application/json',
+'Content-Type': 'application/json',
+},
+body: JSON.stringify({
+
+username: user,
+password: pass
+
+})
+
+}).then((response) => response.json())
+  .then((responseJson) => {
+
+   if(responseJson === 1)
+    {
+        this.storeData();
+        this.props.navigation.navigate('HomeScreen', { username: user });
+
+    }
+    else{
+
+      Alert.alert(responseJson);
+    }
+
+  }).catch((error) => {
+    console.error(error);
+  });
+} else {
+        
+  this.setState(() => ({ nameError: "username or password can not be empty!" }));
+}
+
+}
+
+  
+     
+  render() {
+    return (
+<Container>
+                <Content>
+                <View style={styles.container}>
+               
+                <Text style={{ color: "red" }}>{this.state.nameError}</Text>
+                <Text>Username:</Text>
+                
+                <TextInput placeholder="Enter Username" onChangeText={user => this.setState({user})} style={styles.TextInputStyle}/>
+                
+                <Text>Password:</Text>
+                
+                <TextInput placeholder="Enter User Password" onChangeText={pass => this.setState({pass})} style={styles.TextInputStyle} secureTextEntry={true}/>
+                
+                <Button light block style={{aligSelf:'center'}}  onPress={this.create}><Text>Create</Text></Button>
+                
+                <Text style={{paddingTop:50}}>Already have Account?  <Text onPress={()=>this.props.navigation.navigate('LoginScreen')} style={{color:'#0054FF'}}>Login</Text></Text>
+ 
+ </View>
+                </Content>
+            </Container>
+    );
+  }
+}
+export default RegisterScreen
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+ 
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop:50
+  },
+  TextInputStyle: {
+ 
+    textAlign: 'center',
+    marginBottom: 7,
+    height: 40,
+    width:300,
+    borderWidth: 0,
+    borderColor:'black',
+    backgroundColor:'#E1E1E1',
+    color:'black',
+    
+   
+     
+    },
+     
+     TextComponentStyle: {
+      fontSize: 20,
+      color: "#000",
+      textAlign: 'center', 
+      marginBottom: 15
+     }
+});
